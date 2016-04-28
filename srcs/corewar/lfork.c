a2f3c060a791c6b307f7b07ff063d4fc7ee28669
Florent GIRAUD <fgiraud@e1r8p1.42.fr>
2016-04-28 13:45:46 +0200

    add pseudo code functions

---
+++ srcs/corewar/lfork.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lfork.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgiraud <fgiraud@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/28 13:17:51 by fgiraud           #+#    #+#             */
+/*   Updated: 2016/04/28 13:23:05 by fgiraud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	lfork(struct process, index)
+{
+	//pas de modulo IDX_MOD ici;
+	function create process(process, (process->pc + (index));
+}
