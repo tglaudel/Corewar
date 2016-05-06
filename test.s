ac933525d50a28724a6749da62004e0dd2d319ff
Florent GIRAUD <fgiraud@e1r8p5.42.fr>
2016-05-06 16:05:55 +0200

    modif char

--- test.s
+++ test.s
@@ -1,8 +1,6 @@
 .name "le petit test"
 .comment "flo la tarlouze"
 
-#24 + (-40 mod IDX_MOD = 472)
-
 player:
 	add r1, r7, r14
 	st 	r14, 6
@@ -11,10 +9,43 @@ live:
 	live %:player
 	live %:player
 
+ld:
+	ld %7, r7
+
+
+st r7, -70
+st r7, -79
+st r7, -88
+st r7, -97
+st r7, -106
+st r7, -115
+st r7, -124
+st r7, -133
+st r7, -142
+st r7, -151
+st r7, -160
+st r7, -169
+st r7, -178
+st r7, -187
+st r7, -196
+st r7, -205
+st r7, -214
+zjmp %:live
+ld %1, r6
 ld %0, r7
-
-wall:
-	st r7, -40
-
-ld %0, r14
+sti r7, %:ld, r6
+fork %:live
+fork %:test
+fork %:live
+fork %:test
+fork %:test
 zjmp %:player
+
+
+test:
+	ld %0, r13
+	ld %30, r13
+	add r13, r11, r11
+	sti r14, r11, %0
+	ld %0, r13
+	zjmp %:test
