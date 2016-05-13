a7a90fe8a8f8392f917fc7ea22125ae18b642fd8
Alexis LE NAOURESE <ale-naou@e1r7p6.42.fr>
2016-05-13 15:13:41 +0200

    fixing aff.c

--- srcs/corewar/aff.c
+++ srcs/corewar/aff.c
@@ -6,7 +6,7 @@
 /*   By: ale-naou <ale-naou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/28 13:23:46 by fgiraud           #+#    #+#             */
-/*   Updated: 2016/05/13 12:40:54 by ale-naou         ###   ########.fr       */
+/*   Updated: 2016/05/13 15:10:38 by ale-naou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int			aff(t_env *e, t_proc *proc)
 	if (proc->inst.arg[0] > REG_NUMBER)
 		return (0);
 	if (e->verbose & VERBOSE_OP)
-		printf("P %4d | %s r%d", proc->index, "aff", proc->inst.arg[0]);
+		printf("P %4d | %s r%d \n", proc->index, "aff", proc->inst.arg[0]);
 	if (!have_opt('c', e->opt))
-		printf("%c", e->mem[proc.inst[0]]);
+		printf("%c", e->mem[proc->inst.arg[0]] % 256);
 	return (1);
 }
