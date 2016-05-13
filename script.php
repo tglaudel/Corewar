54c8da61ca89b47db842ccdc41fbb33eafbb52f3
Florent GIRAUD <fgiraud@e1r8p3.42.fr>
2016-05-13 15:54:34 +0200

    add script auto

--- script.php
+++ script.php
@@ -1,5 +1,5 @@
 <?php
-
+// test compil
 if ($handle = opendir('unit_test/instructions')) {
     while (false !== ($entry = readdir($handle))) {
 		{
@@ -9,10 +9,39 @@ if ($handle = opendir('unit_test/instructions')) {
 				{
 					echo "$entry :\n";
     				while (false !== ($entry2 = readdir($handle2))) {
-						if (strcmp($entry2, ".") != 0 && strcmp($entry2, "..") != 0
-							&& strstr($entry2, "test_err"))
+						if (strcmp($entry2, ".") != 0 && strcmp($entry2, "..") != 0 && !strstr($entry2, ".cor"))
+								exec("./asm unit_test/instructions/".$entry.'/'.$entry2);
+					}
+				}
+    			closedir($handle2);
+			}
+		}
+    }
+    closedir($handle);
+}
+
+// test dif
+
+echo "\n----------------------------------------------------------------------------------------------\n\n\n";
+echo "test DIFF flo\n\n\n";
+echo "\n----------------------------------------------------------------------------------------------\n\n\n";
+
+if ($handle = opendir('unit_test/instructions')) {
+    while (false !== ($entry = readdir($handle))) {
+		{
+			if (strcmp($entry, ".") != 0 && strcmp($entry, "..") != 0)
+			{
+				if (($handle2 = opendir('unit_test/instructions/'.$entry)) != NULL)
+				{
+					echo "test DIFF on : $entry :\n";
+    				while (false !== ($entry2 = readdir($handle2))) {
+						if (strcmp($entry2, ".") != 0 && strcmp($entry2, "..") != 0 && strstr($entry2, ".cor"))
 						{
-							exec("./asm /unit_test/instructions/".$entry.'/'.$entry2);
+							// echo("./corewar unit_test/instructions/".$entry."/".$entry2." -v 31  > test1\n");
+							// echo("./ressources/corewar unit_test/instructions/".$entry."/".$entry2."  -v 31 > test2\n");
+							exec("./corewar unit_test/instructions/".$entry."/".$entry2." -v 31 -d 100 > test1");
+							exec("./ressources/corewar unit_test/instructions/".$entry."/".$entry2."  -v 31 -d 100 > test2");
+							exec ("diff test1 test2");
 						}
 					}
 				}
@@ -23,21 +52,27 @@ if ($handle = opendir('unit_test/instructions')) {
     closedir($handle);
 }
 
+echo "\n----------------------------------------------------------------------------------------------\n\n\n";
+echo "RM .cor \n\n\n";
+echo "\n----------------------------------------------------------------------------------------------\n\n\n";
+
+exec("rm unit_test/instructions/**/**.cor");
 
 echo "\n----------------------------------------------------------------------------------------------\n\n\n";
-echo "test DIFF\n\n\n";
+echo "test DIFF alexis\n\n\n";
 echo "\n----------------------------------------------------------------------------------------------\n\n\n";
+// test alexis
 
 echo "-------- diff on ./corewar balzac -v 31 --------\n";
 exec("./corewar ressources/champions/examples/balzac.cor -v 31  > test1");
 exec("./ressources/corewar ressources/champions/examples/balzac.cor  -v 31 > test2");
 exec ("diff test1 test2");
 echo "-------- if nothing here diff OK --------\n";
-echo "-------- diff on ./corewar bee_gees -v 31 --------\n";
-exec("./corewar ressources/champions/examples/bee_gees.cor -v 31 > test1");
-exec("./ressources/corewar ressources/champions/examples/bee_gees.cor  -v 31 > test2");
-exec("diff test1 test2");
-echo "-------- if nothing here diff OK --------\n";
+// echo "-------- diff on ./corewar bee_gees -v 31 --------\n";
+// exec("./corewar ressources/champions/examples/bee_gees.cor -v 31 > test1");
+// exec("./ressources/corewar ressources/champions/examples/bee_gees.cor  -v 31 > test2");
+// exec("diff test1 test2");
+// echo "-------- if nothing here diff OK --------\n";
 echo "-------- diff on ./corewar bigzork -v 31 --------\n";
 exec("./corewar ressources/champions/examples/bigzork.cor -v 31  > test1");
 exec("./ressources/corewar ressources/champions/examples/bigzork.cor -v 31 > test2");
